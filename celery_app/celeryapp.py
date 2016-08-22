3ed398d0b00530a979f73ef4b927d3095d62dc28
kjam <kjarmul@gmail.com>
2016-08-22 16:00:28 +0200

    updating for celerybeat

--- celery_app/celeryapp.py
+++ celery_app/celeryapp.py
@@ -2,12 +2,14 @@
 from celery import Celery
 from kombu import Queue
 from configparser import ConfigParser
+from datetime import datetime, timedelta
 import os
 
 
 
 config = ConfigParser()
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
 if os.environ.get('DEPLOY') == 'PROD':
     config.read(os.path.join(current_dir, 'config/prod.cfg'))
 else:
@@ -17,6 +19,7 @@ app = Celery('tasks', broker=config.get('celery', 'broker_url'))
 
 CELERY_CONFIG = {
     'CELERY_IMPORTS': ['tasks'],
+    'CELERY_TIMEZONE': 'Europe/Berlin',
     'CELERY_IGNORE_RESULT': False,
     'CELERY_TRACK_STARTED': True,
     'CELERY_DEFAULT_QUEUE': 'default',
@@ -31,7 +34,14 @@ CELERY_CONFIG = {
     'CELERY_ANNOTATIONS': {
         'celery.chord_unlock': {'hard_time_limit': 360},
     },
-
+    'CELERYBEAT_SCHEDULE': {
+        'get_stock_info_60s': {
+            'task': 'tasks.get_stock_info',
+            'schedule': timedelta(seconds=60),
+            'args': ('FB', datetime(2016, 1, 1), datetime.today())
+        }
+    }
 }
+
 
 app.conf.update(**CELERY_CONFIG)
