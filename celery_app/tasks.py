9cd71ffc5484456f3490e21e500087b2e5269418
kjam <kjarmul@gmail.com>
2016-08-22 10:54:40 +0200

    change to dictionary return

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -50,16 +50,27 @@ def price_range(stock, start, end, source='yahoo'):
     kwargs:
         source (optional): str
     returns:
-        string
+        dictionary
     '''
     df = data.DataReader(stock, source, start, end)
     period_high = df['Adj Close'].max()
+    period_mean = df['Adj Close'].mean()
     period_low = df['Adj Close'].min()
+    resp = {
+        'stock': stock,
+        'period_high': period_high,
+        'period_low': period_low,
+        'period_mean': period_mean,
+        'period_start': start,
+        'period_end': end,
+    }
     url = 'http://finance.yahoo.com/d/quotes.csv?s={}&f=sat1'.format(stock)
     td = pd.read_csv(url, names=['Stock', 'Price', 'Last Trade'])
     td_price = td['Price'].mean()
+    resp['todays_price'] = td_price
     if abs(td_price - period_high) < abs(td_price - period_low):
-        return 'Price is higher: {}% change from period high ({})'.format(
-            calc_ratio(td_price, period_high), period_high)
-    return 'Price is lower: {}% change from period low ({})'.format(
-        calc_ratio(td_price, period_low), period_low)
+        resp['result'] = 'higher'
+    else:
+        resp['result'] = 'lower'
+    resp['percent_change'] = calc_ratio(td_price, period_mean)
+    return resp
